cde7ef7d949cb00c3024e424224d61306035a01f
Hai-Nyzhnyk Olga <O.Hai-Nyzhnyk@gazer.com>
2019-09-03 11:33:51 +0300

    server

--- server.js
+++ server.js
@@ -5,3 +5,21 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type,authorization,Access-Control-Request-Headers,access-control-allow-headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+app.get("/", function (req, res) {
+    return res.json({
+        message: "UI API!"
+    });
+});
